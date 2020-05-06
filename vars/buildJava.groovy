11165305f5557f5396420f654ffdeab8e080abc9
ravi-avula <ravi.avula@publicissapient.com>
2020-05-06 20:52:28 +0100

    Ravi Avula #comment Add shared libs files #Reviewer Ravi

--- vars/buildJava.groovy
+++ vars/buildJava.groovy
@@ -1,14 +1,14 @@
 /* write the groovy code here */
 def AuthorName="Ravi Avula"
 def call(Map config=[:], Closure body) {
-      {
+    node {
         git url: "https://github.com/ravula022/java-demo"
 
         stage("Build") {
             sh "echo Build the Java project ::"
         }
         stage("Test") {
-            sh "echo test the Java project ::"
+            sh "echo test the Java project::"
         }
 
         body()
