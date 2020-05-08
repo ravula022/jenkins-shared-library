dd9bb9b44fc1572bcc038300616e3d4b5a2cfa0c
ravi-avula <ravi.avula@publicissapient.com>
2020-05-08 13:07:32 +0100

    Ravi Avula #comment Add shared libs files #Reviewer Ravi

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,14 +1,13 @@
 #!/usr/bin/env groovy
 import com.jenkins.libs.GlobalVars;
-def call(String name = 'human') {
+def call(String name = 'human' String filePath) {
   echo "Hello, ${name}."
   //GlobalVars.
   //test()
   //println GlobalVars.foo
+  echo "file Path :: ${filePath}"
+  ut = new GlobalVars()
+  ut.buildPath(filePath)
 }
 
-def call(String filePath = '/home/webspere'){
-    echo "file Path :: ${filePath}"
-    ut = new GlobalVars()
-    ut.buildPath(filePath)
-}
+
