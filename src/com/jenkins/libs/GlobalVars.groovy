e4c4ae0ae4addc7c1475d02dbe0a8594e6acc922
ravi-avula <ravi.avula@publicissapient.com>
2020-05-08 01:41:31 +0100

    Ravi Avula #comment Add shared libs files #Reviewer Ravi

--- src/com/jenkins/libs/GlobalVars.groovy
+++ src/com/jenkins/libs/GlobalVars.groovy
@@ -3,5 +3,5 @@ package com.jenkins.libs;
 
 class GlobalVars {
    static String foo = "bar"
-   println "This is from SRC folder :: $GlobalVars.foo"
+   println "This is from SRC folder :: $foo"
 }
