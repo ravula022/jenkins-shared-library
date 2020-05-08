da5298e7b5822ee9100c430d57ee5ba85f9f2d48
ravi-avula <ravi.avula@publicissapient.com>
2020-05-08 01:19:00 +0100

    Ravi Avula #comment Add shared libs files #Reviewer Ravi

---
+++ src/com/jenkins/libs/GlobalVars.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+package com.jenkins.libs;
+
+class GlobalVars {
+   static String foo = "bar"
+
+   // refer to this in a pipeline using:
+   //
+   // import com.cleverbuilder.GlobalVars
+   // println GlobalVars.foo
+}
--- vars/buildJava.groovy
+++ vars/buildJava.groovy
@@ -2,7 +2,7 @@
 def AuthorName="Ravi Avula"
 def call(Map config=[:], Closure body) {
     node {
-        git url: "https://github.com/ravula022/java-demo"
+        git url: "https://github.com/ravula022/java-demo.git"
 
         stage("Build") {
             sh "echo Build the Java project ::"
