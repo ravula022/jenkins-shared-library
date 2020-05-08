d55978f1267eb4872dfb075e494420427761ed9a
ravi-avula <ravi.avula@publicissapient.com>
2020-05-08 01:37:52 +0100

    Ravi Avula #comment Add shared libs files #Reviewer Ravi

--- src/com/jenkins/libs/GlobalVars.groovy
+++ src/com/jenkins/libs/GlobalVars.groovy
@@ -3,9 +3,5 @@ package com.jenkins.libs;
 
 class GlobalVars {
    static String foo = "bar"
-
-   // refer to this in a pipeline using:
-   //
-   // import com.cleverbuilder.GlobalVars
-   println ("This is from SRC folder" +GlobalVars.foo)
+   println("This is from SRC folder ::" GlobalVars.foo);
 }
